651d2f604f53f32a62668072d6723456822ad04e
Dongmin Yu <ydm2790@gmail.com>
2023-11-01 04:52:48 +0900

    docs: 💫 템플릿 최적화

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Website
 
-This website is built using [Docusaurus 2](https://docusaurus.io/), a modern static website generator.
+이 웹사이트는 최신 정적 웹사이트 생성기인 [Docusaurus 2](https://docusaurus.io/)를 사용하여 제작되었습니다.
 
 ## Installation
 
@@ -14,7 +14,7 @@ yarn
 yarn start
 ```
 
-This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
+이 명령은 로컬 개발 서버를 시작하고 브라우저 창을 엽니다. 대부분의 변경 사항은 서버를 다시 시작할 필요 없이 실시간으로 반영됩니다.
 
 ## Build
 
@@ -22,7 +22,7 @@ This command starts a local development server and opens up a browser window. Mo
 yarn build
 ```
 
-This command generates static content into the `build` directory and can be served using any static contents hosting service.
+이 명령은 정적 콘텐츠를 `build` 디렉터리에 생성하며 모든 정적 콘텐츠 호스팅 서비스를 사용하여 제공할 수 있습니다.
 
 ## Deployment
 
@@ -38,4 +38,4 @@ Not using SSH:
 GIT_USER=<Your GitHub username> yarn deploy
 ```
 
-If you are using GitHub pages for hosting, this command is a convenient way to build the website and push to the `gh-pages` branch.
+호스팅에 GitHub 페이지를 사용하는 경우 이 명령은 웹사이트를 빌드하고 `gh-pages` 브랜치로 푸시하는 편리한 방법입니다.
--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,49 +1,50 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import names from "clsx";
+import styles from "./styles.module.css";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Easy to Use",
+    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Docusaurus는 처음부터 쉽게 설치하도록 설계되었으며 웹 사이트를 신속하게
+        실행하는 데 사용되었습니다.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "Focus on What Matters",
+    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Docusaurus를 사용하면 문서에 집중할 수 있으며 더 중요한 일을 할 수
+        있습니다. 계속해서 문서를 <code>docs</code> 디렉토리로 옮기십시오.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Powered by React",
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        React를 재사용하여 웹 사이트 레이아웃을 확장하거나 사용자 정의하십시오.
+        Docusaurus는 동일한 헤더와 바닥 글을 재사용하는 동안 연장 할 수
+        있습니다.
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={names("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
